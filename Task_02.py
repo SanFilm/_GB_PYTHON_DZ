96a19b32c341c731d21cce3679993e5e1019e440
SanFilm <SanFilm@gmail.com>
2023-05-29 23:53:13 +0300

    Task_01 and Task_02 completed.

--- Task_02.py
+++ Task_02.py
@@ -1,21 +1,17 @@
 print()
 print('- - - - - - - - - - - - - - - - - - ')
-# print('--- Найдите сумму цифр трехзначного числа. ---')
+print('--- Найдите сумму цифр введённого (трехзначного) числа. ---')
 print('- - - - - - - - - - - - - - - - - - ')
 print()
-# n = int(input('Введите число: '))
-n = '321'
-print(type(n))
+n = int(input('Введите число: '))
+# n = 321
 
-summa = 0
-for i in n:
-  print(int(i))
-  # summa += int(n[i])
-  
+rez = 0
+while n > 0:
+  m = n % 10
+  rez += m
+  n //= 10
 
 print()
-print(n)
-print(type(n))
-print(summa)
-# print(f'... {rez} парт требуется для 3-х классов.')
+print(f'Сумма цифр введённого числа = {rez}')
 print()
